dff37b9354b998932718c8f69f66edc54360bcd8
József Tóth-Gerendás <jozsef.toth-gerendas@opswat.com>
2018-09-28 14:07:53 +0200

    add base functionality

--- test.cpp
+++ test.cpp
@@ -3,7 +3,15 @@
 using namespace std;
 
 int main(){
-	cout << "Ez egy teszt program!" << endl;
+	cout << "Adjon meg két számot!" << endl;
+
+	int number1,number2;
+	cout << "Szám 1 : ";
+	cin >> number1;
+	cout << "Szám 2 : ";
+	cin >> number2;
+
+	cout << "A két szám különbsége: " << number1 - number2 << endl;
 
 	return;
 }
