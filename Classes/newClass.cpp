b7bea4095295e2d1c0bcc1d1c6a3bf51451f9627
József Tóth-Gerendás <jozsef.toth-gerendas@opswat.com>
2018-10-01 14:29:24 +0200

    newClass has been completed

---
+++ Classes/newClass.cpp
@@ -0,0 +1,12 @@
+#include "newClass.h"
+
+NewClass::NewClass(int x,int y)
+{
+  	this.x = x;
+  	this.y = y;
+}
+
+int NewClass::getSum()
+{
+  	return x + y;
+}
---
+++ Classes/newClass.h
@@ -0,0 +1,13 @@
+#ifndef NewClass_H
+#define NewClass_H
+
+class NewClass
+{
+	int x = 0;
+ 	int y = 0;
+public:
+  	NewClass(int x,int y);
+  	int getSub();
+};
+
+#endif
